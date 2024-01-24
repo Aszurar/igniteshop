681d678060586b30787b9f67360834d744a29ce9
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-01-24 20:05:28 -0300

    :art: Refactor header styling in _app.tsx :art:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,13 @@ import Logo from '@/assets/icons/logo.svg'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className='bg-zinc-900 flex flex-col items-start justify-center min-h-screen'>
-
-        <header className='pt-10 pb-8 max-w-6xl.5 mx-auto'>
-          <Image src={Logo} width={130} height={52} alt="Ignite Shop"/>
-        </header>
+    <div
+      className={`flex min-h-screen flex-col items-start justify-center 
+          bg-zinc-900 antialiased`}
+    >
+      <header className="mx-auto w-full max-w-6xl.5 pb-8 pt-10">
+        <Image src={Logo} width={130} height={52} alt="Ignite Shop" />
+      </header>
 
       <Component {...pageProps} />
     </div>
